535a7828bea58475bd5a22d211e402b5c39a1214
okor mark <okormark@gmail.com>
2023-07-15 07:42:07 +0100

    fixing

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,40 @@
 #!/usr/bin/node
-const request = require('request');
+/**
+ * Script for printing out all Star Wars Characters
+ */
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+const request = require('request');
+const id = process.argv[2];
+if (!id || isNaN(id)) {
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    // for (const character of characters) {
-    //   request(character, function (error, response, body) {
-    //     if (error) {
-    //       console.log(error);
-    //     } else {
-    //       console.log(JSON.parse(body).name);
-    //     }
-    //   });
-    // }
-    printCharacters(characters, 0);
+    return;
   }
-});
 
-function printCharacters (characters, index) {
-  request(characters[index], function (err, response, body) {
-    if (!err) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
+  const responses = [];
+
+  const json = JSON.parse(body);
+  const characters = json.characters;
+
+  characters.forEach((character) => {
+    const url = character;
+    const promise = new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(JSON.parse(body).name);
+      });
+    });
+    responses.push(promise);
   });
-}
+  Promise.all(responses)
+    .then(names => console.log(names.join('\n')))
+    .catch(errors => console.log(errors));
+});
